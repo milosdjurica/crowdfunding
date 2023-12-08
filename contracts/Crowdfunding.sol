bc28b0a6c4d1e51eec579478563e1be02836be20
milosdjurica <milosdjurica13@gmail.com>
2023-12-08 11:22:25 +0100

    added conventions and started writting contract

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -2,5 +2,20 @@
 pragma solidity ^0.8.20;
 
 contract Crowdfunding {
+    struct Campaign {
+        address owner;
+        string title;
+        string description;
+        uint target;
+        uint deadline;
+        uint amountCollected;
+        string image;
+        address[] donators;
+        uint[] donations;
+    }
+
+    mapping(uint => Campaign) public campaigns;
+    uint public numberOfCampaigns;
+
     constructor() {}
 }
